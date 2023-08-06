e75107d1b84d0346cb21801a352b4f52115a8331
Kalkidan <kalkidanmulu12@gmail.com>
2023-08-06 05:54:14 +0300

    Nav-Bar-Updated-With remaning functinalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import NacBarr from './components/Navigation/NacBarr';
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+      <NacBarr/>
     </div>
   );
 }
